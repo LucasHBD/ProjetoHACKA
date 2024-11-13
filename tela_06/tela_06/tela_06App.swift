fb309f2dc120c2e7e723420950496118e36ff21a
Anderson Henrique <144293720+anderson-henriqq@users.noreply.github.com>
2024-11-12 21:21:25 -0300

    Add files via upload

---
+++ tela_06/tela_06/tela_06App.swift
@@ -0,0 +1,17 @@
+//
+//  tela_06App.swift
+//  tela_06
+//
+//  Created by Turma02-1 on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct tela_06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
