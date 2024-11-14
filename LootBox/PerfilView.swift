f4313ee5825c0b0ed3ba080e2a3bf09e86b1c24d
Anderson Henrique <144293720+anderson-henriqq@users.noreply.github.com>
2024-11-13 21:01:32 -0300

    Update PerfilView.swift

--- LootBox/PerfilView.swift
+++ LootBox/PerfilView.swift
@@ -9,7 +9,77 @@ import SwiftUI
 
 struct PerfilView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                HStack{
+                    Spacer()
+                    Spacer()
+                    Image("controle")
+                        .padding(.leading, 30)
+                        .padding(.bottom)
+                        .ignoresSafeArea()
+                        .offset(x: 20, y:0)
+                    Spacer()
+                    Image("sino")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .ignoresSafeArea()
+                        .padding(.bottom, 50)
+                        .offset(x:20, y: -12.5)
+                    Image("perfil")
+                        .resizable()
+                        .frame(width:30, height: 30)
+                        .ignoresSafeArea()
+                        .padding(.bottom, 50)
+                        .offset(x:30, y: -12.5)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                ZStack{
+                    Rectangle()
+                        .fill(.rose)
+                        .frame(width: 285, height: 350)
+                        .cornerRadius(10)
+                    
+                    VStack{
+                        
+                        VStack{
+                            Rectangle()
+                                .fill(.white)
+                                .frame(width: 138, height: 176)
+                                .cornerRadius(10)
+                            VStack{
+                                VStack(alignment: .center){
+                                    Text("Nome:")
+                                        .foregroundColor(.white)
+                                    Text("Preço:")
+                                        .foregroundColor(.white)
+                                    Text("Site:")
+                                        .foregroundColor(.white)
+                                    
+                                }
+                                .font(.custom("Jomhuria-Regular", size: 30 ))
+                                
+                                Text("VER JOGO")
+                                        .background(.roxoEscuro)
+                                        .cornerRadius(5)
+                                        .foregroundColor(.white)
+                                        .font(.custom("Jomhuria-Regular", size: 34 ))
+                            }
+                        
+                        }
+                    }
+                }
+                Spacer()
+                Spacer()
+            }
+        }
     }
 }
 
